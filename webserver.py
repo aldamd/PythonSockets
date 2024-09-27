affba401d3f15278d7a7db1e64d62a4245814e9c
Daniel Aldam <danaldam6@gmail.com>
2024-09-27 12:03:54 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,39 +1,38 @@
-from socket import *
-import sys
+from socket import socket, AF_INET, SOCK_STREAM
+from sys import exit as sysexit
 
-def webServer(port=6789):
-    serverSocket = socket(AF_INET, SOCK_STREAM)
+def webServer(port: int=6789) -> None:
+    with socket(AF_INET, SOCK_STREAM) as serverSocket:
 
-    serverSocket.bind(("", port)) #anyone can connect
-    serverSocket.listen() #listen to a request from the server
+        serverSocket.bind(("", port)) #anyone can connect
+        serverSocket.listen() #listen to a request from the server
 
-    while True:
-        print("Ready to serve...")
-        connectionSocket, addr = serverSocket.accept()
-        try:
-            message = connectionSocket.recv(1024)
-            filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata = f.read()
+        while True:
+            print("Ready to serve...")
+            connectionSocket, _ = serverSocket.accept()
+            try:
+                message = connectionSocket.recv(1024)
+                filename = message.split()[1]
+                with open(filename[1:]) as r:
+                    outputdata = r.read()
 
-            #Send one HTTP header line into socket
-            connectionSocket.send("HTTP/1.0 200 OK\r\n\r\n".encode())
+                #Send one HTTP header line into socket
+                connectionSocket.send("HTTP/1.0 200 OK\r\n\r\n".encode())
 
-            for i in range(len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
+                for i in range(len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
 
-            connectionSocket.close()
-        except IOError:
-            #Send response message for file not found
-            connectionSocket.send("HTTP/1.0 404 Not Found\r\n\r\n".encode())
+                connectionSocket.close()
+            except IOError:
+                #Send response message for file not found
+                connectionSocket.send("HTTP/1.0 404 Not Found\r\n\r\n".encode())
 
-            #Close client socket
-            connectionSocket.close()
-        except KeyboardInterrupt:
-            break
+                #Close client socket
+                connectionSocket.close()
+            except KeyboardInterrupt:
+                break
 
-    serverSocket.close()
-    sys.exit()
+    sysexit()
 
 if __name__ == "__main__":
     webServer(6789)
